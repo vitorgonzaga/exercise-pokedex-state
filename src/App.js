3f9f83f188a7a9653d7a436919102de0fb2549a4
Felipe Flores <felipefloresvendas@gmail.com>
2021-04-30 10:20:25 -0300

    configuration

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
 import React from 'react';
-import Botao from './Botao';
 import Pokedex from './Pokedex';
 import pokemonData from './data';
 
 
 function App() {
   return (
-
     <div>
-     <Pokedex pok= { pokemonData }/>
+     <Pokedex pokemons= { pokemonData }/>
     </div>
   );
 }
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,40 +1,69 @@
 import PropTypes from 'prop-types';
 import Pokemon from './Pokemon';
 import React from 'react';
+import data from './data';
+import './PokedexCSS.css'
 
 class Pokedex extends React.Component {
   constructor(props) {
     super(props)
+
+    this.types = ['All', ...new Set(this.props.pokemons.map(pokemon => pokemon.type))].sort();
     this.state = {
+      // pokemonsTypes: data,
       position: 0,
-    }
+
+    };
+
     this.nextPokemon = this.nextPokemon.bind(this);
+      this.pokemonsByType = this.pokemonsByType.bind(this);
   }
 
   nextPokemon() {
-    const { pok } = this.props;
+    const { pokemons } = this.props;
     this.setState((previousState, _props) => {
       const { position } = previousState;
-      const indexAtual = position === pok.length - 1 ? 0 : position + 1;
-      return { ...previousState, position: indexAtual }
+      const indexAtual = position === pokemons.length - 1 ? 0 : position + 1;
+      return { ...previousState, position: indexAtual };
     }
     )
+  }
+
+  pokemonsByType(event) {
+
+    const currentType = event.target.id;
+    const filtredsPokemons = data.filter((pokemon) => pokemon.type === currentType);
+    console.log(filtredsPokemons);
+    return filtredsPokemons.map((pokemon, index) => <Pokemon poke={pokemon[index]} />)
+     
+    // return this.props = filtredsPokemons;
+    // this.setState((previousState, _props) => {
+    //   const filtredsPokemons = data.filter((pokemon) => pokemon.type === currentType);
+    //   const { pokemonsTypes } = previousState;
+    //   return { ...previousState,  pokemonsTypes: filtredsPokemons };
+    // })
+
   }
 
   render() {
     return (
       <>
         <div className="App">
-          <Pokemon poke={this.props.pok[this.state.position]} />
+          <Pokemon poke={this.props.pokemons[this.state.position]} />
         </div>
-        <button onClick={this.nextPokemon}>Next Podemon!</button>
+        <div>
+          {this.types.map((tipo) => <button key={tipo} id={tipo} onClick={ (event) => this.pokemonsByType(event) }>{tipo}</button>)}
+        </div>
+        <div>
+        <button className="btn-next row-btn-next" onClick={this.nextPokemon}>Next Pokemon!</button>
+        </div>
       </>
     )
   }
 }
 
 Pokedex.propTypes = {
-  pok: PropTypes.arrayOf(PropTypes.shape({
+  pokemon: PropTypes.arrayOf(PropTypes.shape({
     name: PropTypes.string.isRequired,
     type: PropTypes.string.isRequired,
     averageWeight: PropTypes.shape({
